b45e24f470f10583581eba7e1e8352873f6d6c8a
xlcodes <xiaolin201809@126.com>
2025-04-12 13:23:01 +0800

    test: 完善 role 单元测试

--- src/modules/role/role.service.spec.ts
+++ src/modules/role/role.service.spec.ts
@@ -7,7 +7,8 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { PermissionEntity } from '@/modules/permission/entities/permission.entity';
 import { ResultData } from '@/core/utils/result';
 import { ResultCodeEnum } from '@/core/common/constant';
-import { In } from 'typeorm';
+import { In, IsNull } from 'typeorm';
+import { TEST_PAGE_NO, TEST_PAGE_SIZE, TEST_TOTAL } from '@/test/test.constant';
 
 describe('RoleService', () => {
   let service: RoleService;
@@ -25,7 +26,8 @@ describe('RoleService', () => {
       description: 'test-description',
     },
   ];
-  const TEST_ERROR = new Error('test-error');
+
+  const TEST_ERROR = new Error('role-service-test-error');
 
   const createMockDto = (options = {}) => {
     return {
@@ -36,13 +38,29 @@ describe('RoleService', () => {
     };
   };
 
-  const createMockRoleRes = () =>
+  const createMockRoleRes = (options = {}) =>
     createMockDto({
       id: TEST_ROLE_ID,
       createdAt: TEST_DATE,
       updatedAt: TEST_DATE,
       deletedAt: null,
+      ...options,
     });
+
+  const mockRoleData = () => ({
+    list: [
+      createMockRoleRes({ id: 1 }),
+      createMockRoleRes({ id: 2 }),
+      createMockRoleRes({ id: 3 }),
+      createMockRoleRes({ id: 4 }),
+      createMockRoleRes({ id: 5 }),
+    ],
+    pageInfo: {
+      pageNo: TEST_PAGE_NO,
+      pageSize: TEST_PAGE_SIZE,
+      total: TEST_TOTAL,
+    },
+  });
 
   const mockUserService = {
     validateUser: jest.fn(),
@@ -402,6 +420,124 @@ describe('RoleService', () => {
       expect(loggerSpy).toHaveBeenCalledWith(TEST_ERROR);
     });
   });
-  describe('findAll 查询角色分页', () => {});
-  describe('findOne 查询角色详情', () => {});
+
+  describe('findAll 查询角色分页', () => {
+    beforeEach(() => {
+      mockRoleRepo.findOne.mockResolvedValue(createMockRoleRes());
+    });
+
+    it('should return not user when user not found', () => {
+      validateUser(async () => {
+        return service.findAll(TEST_PAGE_NO, TEST_PAGE_SIZE, TEST_USER_ID);
+      });
+    });
+
+    it('should return error when find role is error', async () => {
+      mockRoleRepo.findAndCount.mockImplementation(() => {
+        throw TEST_ERROR;
+      });
+
+      const loggerSpy = jest.spyOn(service['logger'], 'error');
+
+      const result = await service.findAll(
+        TEST_PAGE_NO,
+        TEST_PAGE_SIZE,
+        TEST_USER_ID,
+      );
+
+      expect(result).toEqual({
+        code: ResultCodeEnum.error,
+        message: '查询角色失败',
+        data: undefined,
+      });
+
+      expect(loggerSpy).toHaveBeenCalledWith(TEST_ERROR);
+    });
+
+    it('should return role list', async () => {
+      const mockData = mockRoleData();
+
+      mockRoleRepo.findAndCount.mockResolvedValue([
+        mockData.list,
+        mockData.pageInfo.total,
+      ]);
+
+      const result = await service.findAll(
+        TEST_PAGE_NO,
+        TEST_PAGE_SIZE,
+        TEST_USER_ID,
+      );
+
+      expect(result).toEqual({
+        code: ResultCodeEnum.success,
+        message: '查询角色成功',
+        data: mockData,
+      });
+
+      expect(mockRoleRepo.findAndCount).toHaveBeenCalledWith({
+        where: {
+          deletedAt: IsNull(),
+        },
+        skip: (TEST_PAGE_NO - 1) * TEST_PAGE_SIZE,
+        take: TEST_PAGE_SIZE,
+        select: ['id', 'name', 'description', 'createdAt', 'updatedAt'],
+      });
+    });
+  });
+
+  describe('findOne 查询角色详情', () => {
+    beforeEach(() => {
+      mockRoleRepo.findOne.mockResolvedValue(createMockRoleRes());
+    });
+
+    it('should return not user when user not found', () => {
+      validateUser(async () => {
+        return service.findOne(TEST_ROLE_ID, TEST_USER_ID);
+      });
+    });
+
+    it('should return error when find role is not found', async () => {
+      mockRoleRepo.findOne.mockResolvedValue(null);
+
+      const result = await service.findOne(TEST_ROLE_ID, TEST_USER_ID);
+
+      expect(result).toEqual({
+        code: ResultCodeEnum.exception_error,
+        message: '当前角色不存在',
+        data: undefined,
+      });
+    });
+
+    it('should return error when find role is error', async () => {
+      mockRoleRepo.findOne.mockImplementation(() => {
+        throw TEST_ERROR;
+      });
+
+      const loggerSpy = jest.spyOn(service['logger'], 'error');
+
+      const result = await service.findOne(TEST_ROLE_ID, TEST_USER_ID);
+
+      expect(result).toEqual({
+        code: ResultCodeEnum.error,
+        message: '查询角色失败',
+        data: undefined,
+      });
+
+      expect(loggerSpy).toHaveBeenCalledWith(TEST_ERROR);
+    });
+
+    it('should return role detail', async () => {
+      const mockData = createMockRoleRes();
+
+      mockRoleRepo.findOne.mockResolvedValue(mockData);
+
+      const result = await service.findOne(TEST_ROLE_ID, TEST_USER_ID);
+
+      expect(result).toEqual({
+        code: ResultCodeEnum.success,
+        message: `查询角色成功`,
+        data: mockData,
+      });
+    });
+  });
 });
